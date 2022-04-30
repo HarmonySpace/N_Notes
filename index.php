d8a441e2048eeb2f177ef6fd773c169854949ef8
HarmonySpace <jchavaspace@gmail.com>
2022-04-30 09:54:51 -0600

    v 0.02 -> start

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <section class="sec-container-1">
+        <nav class="nav-container-1">
+            <ul class="list-container-1">
+                <li class="list-element-1">Nueva</li>
+                <li class="list-element-1">Notas</li>
+                <li class="list-element-1">Listas</li>
+                <li class="list-element-1">Sign in</li>
+                <li class="list-element-1">Log in</li>
+            </ul>
+        </nav>      
+    </section>
+</body>
+</html> 
+
