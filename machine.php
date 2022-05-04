4f3b470074bc72a8076c7e6610485762677067ca
HarmonySpace <jchavaspace@gmail.com>
2022-05-04 14:07:58 -0600

    v 0.06 -> >storage functions<

--- machine.php
+++ machine.php
@@ -3,15 +3,22 @@
     include_once 'user.php';
 
     if(isset($_POST["crear"])){
-        $us = new UserCred($_POST["username"], $_POST["correo"], $_POST["password"]);
+        $list = Storages::pusheadatos();
+        try{
+            $us = new UserCred($_POST["username"], $_POST["correo"], $_POST["password"]);
+        }
+        catch(Exception $e){
+            echo "ERROR: ".$e->getMessage();
+        }
+        array_push($list, $us);
+        $result = Storages::ingresardatos($list);
+        if ($result){
+            
+        }
+
     }else{
         echo "<h1>NO ONE HERE</h1>";
     }
-
-
-
-
-
 
 
 
